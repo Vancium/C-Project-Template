115c96a405042fa59dcbe9d6d7ab4ef22e208974
Vancium <Natevance01@gmail.com>
2024-01-04 18:20:20 -0600

    C / C++ Project Template

---
+++ .nvim/nvim-dap.lua
@@ -0,0 +1,20 @@
+local dap = require("dap")
+
+dap.adapters.lldb = {
+	name = "lldb",
+	type = "executable",
+	command = "/opt/homebrew/opt/llvm/bin/lldb-vscode",
+}
+dap.configurations.c = {
+	{
+		name = "Launch Debugger",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.getcwd() .. "/out/Debug/test"
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+	},
+}
---
+++ project/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+struct App {
+  char *name;
+  int height;
+  int widht;
+  int weight;
+};
+
+int main() { printf("Hello Project"); }
